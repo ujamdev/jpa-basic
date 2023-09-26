ebf20e1b9b535d1d8caed58959caead6018d2533
ujamdev <ujamdev@gmail.com>
2023-09-26 22:16:42 +0900

    feat: JPQL을 사용하여 페이지네이션 작업 추가

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
@@ -14,15 +12,27 @@ public class Main {
     EntityTransaction tx = em.getTransaction();
     tx.begin();
 
-    Member member = new Member();
-    member.setId(1L);
-    member.setName("HelloA");
+    try {
+      // JPA를 사용하여 업데이트
+      // Member findMember = em.find(Member.class, 1L);
+      // findMember.setName("HelloJPA");
 
-    em.persist(member);
+      // JPQL을 사용하여 페이지네이션
+      List<Member> result = em.createQuery("select m from Member as m", Member.class)
+          .setFirstResult(1)
+          .setMaxResults(10)
+          .getResultList();
 
-    tx.commit();
+      for (Member member : result) {
+        System.out.println("member.name = " + member.getName());
+      }
 
-    em.close();
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
 
     emf.close();
   }
