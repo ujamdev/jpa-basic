cd34e61049ae0fa99fd5ed8f80528d77601112cc
ujamdev <ujamdev@gmail.com>
2023-09-28 11:07:48 +0900

    feat: 1차 캐시 조회 관련 코드 추가

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -16,12 +16,18 @@ public class Main {
       
       //비영속
       Member member = new Member();
-      member.setId(100L);
+      member.setId(101L);
       member.setName("HelloJPA");
 
       //영속 (객체를 저장한 상태)
       em.persist(member);
-      
+
+      //1차 캐시 조회
+      Member findMember = em.find(Member.class, 101L);
+
+      System.out.println("findMember.id = " + findMember.getId());
+      System.out.println("findMember.name = " + findMember.getName());
+
       tx.commit();
     } catch (Exception e) {
       tx.rollback();
