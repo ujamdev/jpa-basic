30d578e88d733e3c94dbb25ee01eef341f9652f4
ujamdev <ujamdev@gmail.com>
2023-10-01 18:25:10 +0900

    feat: Member 필드 컬럼 매핑

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,24 +1,47 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Entity
 public class Member {
 
   @Id
   private Long id;
-  @Column(nullable = false, length = 10)
-  private String name;
+  @Column(name = "name")
+  private String username;
   private int age;
+  @Enumerated(EnumType.STRING)
+  private RoleType roleType;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date createdDate;
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date lastModifiedDate;
 
-  public Member() {
+  private LocalDateTime createdDate2;
+  private LocalDateTime lastModifiedDate2;
+
+  public LocalDateTime getCreatedDate2() {
+    return createdDate2;
   }
 
-  public Member(Long id, String name) {
-    this.id = id;
-    this.name = name;
+  public void setCreatedDate2(LocalDateTime createdDate2) {
+    this.createdDate2 = createdDate2;
+  }
+
+  public LocalDateTime getLastModifiedDate2() {
+    return lastModifiedDate2;
+  }
+
+  public void setLastModifiedDate2(LocalDateTime lastModifiedDate2) {
+    this.lastModifiedDate2 = lastModifiedDate2;
+  }
+
+  @Lob
+  private String description;
+
+  public Member() {
   }
 
   public Long getId() {
@@ -29,11 +52,51 @@ public class Member {
     this.id = id;
   }
 
-  public String getName() {
-    return name;
+  public String getUsername() {
+    return username;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public RoleType getRoleType() {
+    return roleType;
+  }
+
+  public void setRoleType(RoleType roleType) {
+    this.roleType = roleType;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public Date getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(Date lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
   }
 }
