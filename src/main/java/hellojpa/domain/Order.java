255f3306d8380c73a400b3451a812bbf7efcc105
ujamdev <ujamdev@gmail.com>
2023-10-08 10:14:19 +0800

    feat: Delivery 엔티티 구현 및 Order 엔티티와 연관관계 매핑

--- src/main/java/hellojpa/domain/Order.java
+++ src/main/java/hellojpa/domain/Order.java
@@ -15,6 +15,10 @@ public class Order {
   @JoinColumn(name = "MEMBER_ID", insertable = false, updatable = false)
   private Member member;
 
+  @OneToOne
+  @JoinColumn(name = "DELIVERY_ID")
+  private Delivery delivery;
+
   private LocalDateTime orderDate;
   @Enumerated(EnumType.STRING)
   private OrderStatus status;
