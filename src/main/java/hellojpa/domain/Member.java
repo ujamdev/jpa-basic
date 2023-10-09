d3c0ca906636dc4778948c2ffad480025eaae119
ujamdev <ujamdev@gmail.com>
2023-10-09 18:55:29 +0900

    feat: 모든 엔티티에 BaseEntity 상속

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -3,7 +3,7 @@ package hellojpa.domain;
 import javax.persistence.*;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity {
 
   @Id @GeneratedValue
   @Column(name = "MEMBER_ID")
