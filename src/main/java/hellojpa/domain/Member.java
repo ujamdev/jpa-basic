14a7530654ae11a028bc3240c4833d6eb948a69b
ujamdev <ujamdev@gmail.com>
2023-10-19 22:30:50 +0900

    feat: Member 엔티티에 값 타입 컬렉션 favoriteFoods, adressHistory 추가
    Member.java
     - 값 타입을 하나 이상 저장할 때 사용
     - @ElementCollection, @CollectionTable 사용
     - 데이터베이스는 컬렉션을 같은 테이블에 저장할 수 없음
     - 컬렉션을 저장하기 위한 별도의 테이블이 필요

--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -1,6 +1,10 @@
 package hellojpa.domain;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member extends BaseEntity {
@@ -13,6 +17,20 @@ public class Member extends BaseEntity {
 
   @Embedded
   private Address homeAddress;
+
+  @ElementCollection
+  @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+      @JoinColumn(name = "MEMBER_ID")
+  )
+  @Column(name = "FOOD_NAME")
+  private Set<String> favoriteFoods = new HashSet<>();
+
+  @ElementCollection
+  @CollectionTable(name = "ADDRESS", joinColumns =
+      @JoinColumn(name = "MEMBER_ID")
+  )
+  @Column(name = "FOOD_NAME")
+  private List<Address> addressHistory = new ArrayList<>();
 
   public Long getId() {
     return id;
