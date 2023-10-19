0da14132c6ecf9e31fd8ae46d184a72aa9e2e115
ujamdev <ujamdev@gmail.com>
2023-10-19 22:49:34 +0900

    feat: 값 타입 저장, 조회 예제 코드 구성
    Member.java
     - favoriteFoods, addressHistory getter setter 추가
    Main.java
     - 값 타입 저장 예제와 조회 예제 코드 구성
     - 값 타입 컬렉션도 지연 로딩 전략 사용

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -1,15 +1,68 @@
 package hellojpa;
 
 import hellojpa.domain.Address;
+import hellojpa.domain.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Set;
 
 public class Main {
   public static void main(String[] args) {
 
-    Address address1 = new Address("city", "street", "10000");
-    Address address2 = new Address("city", "street", "10000");
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
 
-    System.out.println("address1 == address2: " + (address1 == address2));
-    System.out.println("address1 equals address2: " + (address1.equals(address2)));
+    EntityManager em = emf.createEntityManager();
+
+    EntityTransaction tx = em.getTransaction();
+    tx.begin();
+
+    try {
+
+      // 값타입 저장 예제
+      Member member = new Member();
+      member.setName("member1");
+      member.setHomeAddress(new Address("homeCity","street", "10000"));
+
+      member.getFavoriteFoods().add("치킨");
+      member.getFavoriteFoods().add("피자");
+      member.getFavoriteFoods().add("족발");
+
+      member.getAddressHistory().add(new Address("old1", "street", "10000"));
+      member.getAddressHistory().add(new Address("old2", "street", "10000"));
+
+      em.persist(member);
+
+      em.flush();
+      em.clear();
+
+      System.out.println("=========START==========");
+      Member findMember = em.find(Member.class, member.getId());
+
+      //지연로딩
+      List<Address> addressHistory = findMember.getAddressHistory();
+      for (Address address : addressHistory) {
+        System.out.println("address = " + address.getCity());
+      }
+
+      //지연로딩
+      Set<String> favoriteFoods = findMember.getFavoriteFoods();
+      for (String favoriteFood : favoriteFoods) {
+        System.out.println("favoriteFood = " + favoriteFood);
+      }
+
+
+      tx.commit();
+    } catch (Exception e) {
+      tx.rollback();
+    } finally {
+      em.close();
+    }
+
+    emf.close();
 
   }
 }
--- src/main/java/hellojpa/domain/Member.java
+++ src/main/java/hellojpa/domain/Member.java
@@ -55,4 +55,20 @@ public class Member extends BaseEntity {
   public void setHomeAddress(Address homeAddress) {
     this.homeAddress = homeAddress;
   }
+
+  public Set<String> getFavoriteFoods() {
+    return favoriteFoods;
+  }
+
+  public void setFavoriteFoods(Set<String> favoriteFoods) {
+    this.favoriteFoods = favoriteFoods;
+  }
+
+  public List<Address> getAddressHistory() {
+    return addressHistory;
+  }
+
+  public void setAddressHistory(List<Address> addressHistory) {
+    this.addressHistory = addressHistory;
+  }
 }
