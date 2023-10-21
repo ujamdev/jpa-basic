8ae9e31b6b18d4b34a6a9b7c6a90554b947a7842
ujamdev <ujamdev@gmail.com>
2023-10-21 21:08:43 +0900

    feat: Address 엔티티에 equals, hashCode 추가 및 Setter 추가

--- src/main/java/hellojpa/domain/Address.java
+++ src/main/java/hellojpa/domain/Address.java
@@ -1,18 +1,13 @@
 package hellojpa.domain;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
 public class Address {
   private String city;
   private String street;
   private String zipcode;
-
-  public Address(String city, String street, String zipcode) {
-    this.city = city;
-    this.street = street;
-    this.zipcode = zipcode;
-  }
 
   public String getCity() {
     return city;
@@ -21,8 +16,33 @@ public class Address {
   public String getStreet() {
     return street;
   }
+
   public String getZipcode() {
     return zipcode;
   }
 
+  private void setCity(String city) {
+    this.city = city;
+  }
+
+  private void setStreet(String street) {
+    this.street = street;
+  }
+
+  private void setZipcode(String zipcode) {
+    this.zipcode = zipcode;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Address address = (Address) o;
+    return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getCity(), getStreet(), getZipcode());
+  }
 }
