5eab458345987e9e3a28fea1ac1f3a567558f67f
ujamdev <ujamdev@gmail.com>
2023-10-21 21:10:24 +0900

    feat: Delivery 엔티티에 Address 값 타입 지정 및 Getter, Setter 추가

--- src/main/java/hellojpa/domain/Delivery.java
+++ src/main/java/hellojpa/domain/Delivery.java
@@ -10,10 +10,33 @@ public class Delivery extends BaseEntity {
   @Id @GeneratedValue
   private Long id;
 
-  private String city;
-
-  private String street;
+  @Embedded
+  private Address address;
 
   @OneToOne(fetch = LAZY, mappedBy = "delivery")
   private Order order;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Address getAddress() {
+    return address;
+  }
+
+  public void setAddress(Address address) {
+    this.address = address;
+  }
+
+  public Order getOrder() {
+    return order;
+  }
+
+  public void setOrder(Order order) {
+    this.order = order;
+  }
 }
