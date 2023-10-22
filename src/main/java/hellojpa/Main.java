5bbde31aa9435d79fe7d1f3c211bd2e9d60a46de
ujamdev <ujamdev@gmail.com>
2023-10-22 18:00:52 +0900

    feat: TypeQuery, Query 사용하여 코드 추가
    - TypeQuery는 반환 타입이 명확할 때 사용
    - Query는 반환 타입이 명확하지 않을 때 사용

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -3,10 +3,7 @@ package hellojpa;
 import hellojpa.domain.Address;
 import hellojpa.domain.Member;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import javax.persistence.*;
 import java.util.List;
 import java.util.Set;
 
@@ -22,37 +19,13 @@ public class Main {
 
     try {
 
-      // 값타입 저장 예제
       Member member = new Member();
       member.setName("member1");
-      member.setHomeAddress(new Address("homeCity","street", "10000"));
-
-      member.getFavoriteFoods().add("치킨");
-      member.getFavoriteFoods().add("피자");
-      member.getFavoriteFoods().add("족발");
-
-      member.getAddressHistory().add(new Address("old1", "street", "10000"));
-      member.getAddressHistory().add(new Address("old2", "street", "10000"));
-
       em.persist(member);
 
-      em.flush();
-      em.clear();
-
-      System.out.println("=========START==========");
-      Member findMember = em.find(Member.class, member.getId());
-
-      //지연로딩
-      List<Address> addressHistory = findMember.getAddressHistory();
-      for (Address address : addressHistory) {
-        System.out.println("address = " + address.getCity());
-      }
-
-      //지연로딩
-      Set<String> favoriteFoods = findMember.getFavoriteFoods();
-      for (String favoriteFood : favoriteFoods) {
-        System.out.println("favoriteFood = " + favoriteFood);
-      }
+      TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+      TypedQuery<Member> query2 = em.createQuery("select m.username from Member m", Member.class);
+      Query query3 = em.createQuery("select m.username from Member m");
 
 
       tx.commit();
