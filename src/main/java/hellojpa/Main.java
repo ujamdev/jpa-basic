a0e39d5bd75756e22e7471c41d69fd3fd0f874e6
ujamdev <ujamdev@gmail.com>
2023-10-26 18:26:50 +0900

    feat: 기본 CASE 식 관련 코드 추가

--- src/main/java/hellojpa/Main.java
+++ src/main/java/hellojpa/Main.java
@@ -28,10 +28,10 @@ public class Main {
       em.flush();
       em.clear();
 
-      List<Member> result = em.createQuery("select m from Member m where m.age > (select avg(m2.age) from Member m2)", Member.class)
+      List<Member> result = em.createQuery("select case when m.age <= 19 then '학생요금' when m.age >= 60 then '경로요금' else '일반요금' end from Member m", Member.class)
           .getResultList();
 
-      System.out.println("result.size = " + result.size());
+      System.out.println("result = " + result);
 
       tx.commit();
     } catch (Exception e) {
